1c8e5c93e3371eef62802210fb497086398cbedf
yuu <yuu@users.noreply.github.com>
2020-08-16 17:07:00 +0900

    Add grpc server

---
+++ server/src/main.cpp
@@ -0,0 +1,33 @@
+#include <memory>
+#include <string>
+
+#include <grpc++/grpc++.h>
+
+using grpc::Server;
+using grpc::ServerBuilder;
+// using grpc::ServerContext;
+// using grpc::Status;
+
+void run_server() {
+    std::string server_address("0.0.0.0:50051");
+
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
+    // Register "service" as the instance through which we'll communicate with clients.
+    // In this case it corresponds to an *synchronous* service.
+    // builder.RegisterService(&service);
+
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown.
+    // Note that some other thread must be responsible for shutting down the server for this call to ever return.
+    server->Wait();
+}
+
+int main(int ac, char **av) {
+    run_server();
+}
