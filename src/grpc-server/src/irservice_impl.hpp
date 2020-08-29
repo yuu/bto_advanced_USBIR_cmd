ce5f95412f4b0602ce84817d9fbb7ae3b221edda
yuu <yuu@users.noreply.github.com>
2020-08-29 18:24:37 +0900

    impl other interface

--- src/grpc-server/src/irservice_impl.hpp
+++ src/grpc-server/src/irservice_impl.hpp
@@ -32,6 +32,40 @@ public:
         return Status::OK;
     }
 
+    Status RecStart(ServerContext* context,
+                    const bto::RecStartRequest* request,
+                    bto::RecStartResponse* response) override {
+        const auto ret = ir->rec_start(request->frequency());
+        response->set_code(ret);
+
+        return Status::OK;
+    }
+
+    Status RecStop(ServerContext* context,
+                   const bto::RecStopRequest* request,
+                   bto::RecStopResponse* response) override {
+        const auto ret = ir->rec_stop();
+        response->set_code(ret);
+
+        return Status::OK;
+    }
+
+    Status DumpRecord(ServerContext* context,
+                      const bto::DumpRecordRequest* request,
+                      bto::DumpRecordResponse* response) override {
+        auto data = std::make_unique<byte[]>(MAX_BYTE_ARRAY_SIZE);
+
+        uint actual_size = 0;
+        const auto ret = ir->dump_record(data.get(), MAX_BYTE_ARRAY_SIZE, &actual_size);
+        response->set_code(ret);
+
+        for (int32_t i = 0; i < actual_size; ++i) {
+            response->add_data(data[i]);
+        }
+
+        return Status::OK;
+    }
+
 private:
     std::unique_ptr<IR> ir;
 };
